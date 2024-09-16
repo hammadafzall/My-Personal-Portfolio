e4374f1303c99476c79fbab41bfd9f0dc3fb8419
Hammad Afzal <127996183+hammad42662@users.noreply.github.com>
2024-09-16 02:06:46 +0100

    Update About.jsx

--- src/app/components/tabs/About.jsx
+++ src/app/components/tabs/About.jsx
@@ -7,12 +7,12 @@ export default function About() {
       </h3>
       <div className="mt-4">
         <p className="text-gray-500 text-lg leading-10 ">
-          I’m a Software Developer with nearly two years of experience
+          I’m a Website Developer with nearly two years of experience
           specializing in front-end development and fundamental knowledge of
-          backend technologies. Over the past year as a Freelance Web Developer,
+          backend technologies. Over the past year as a Web Developer,
           I’ve created visually engaging and user-friendly websites for various
           clients. Previously, I spent eight months at Convent Generation IT as
-          a Junior Web Developer, where I worked on responsive web pages and
+          a Junior Frontend Developer, where I worked on responsive web pages and
           collaborated closely with senior developers on key projects. I’m
           passionate about building seamless digital experiences and am
           committed to delivering high-quality work that meets both client and
