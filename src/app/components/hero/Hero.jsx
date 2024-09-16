a4ea90065b44602f39dc88aeba8e1df33a97e0c3
Hammad Afzal <127996183+hammad42662@users.noreply.github.com>
2024-09-16 02:05:26 +0100

    Update Hero.jsx

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -17,10 +17,10 @@ export default function Hero() {
         <h1 className="text-3xl">Hi, I am</h1>
         <TypeWriter text="HAAMMAD AFZAL" speed={300} />
         <p className="text-center">
-          A Software Developer and Digital Marketer.
+          A Website Developer.
         </p>
         <p className="text-xl text-center">
-          I build interactive <span className="text-blue-500">Websites</span>{" "}
+          I build interactive and responsive <span className="text-blue-500">Websites</span>{" "}
           that run across platforms and devices.
         </p>
       </div>
