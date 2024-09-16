1c34ba397b135cadb7d0951a74c62445bb34d432
Hammad Afzal <127996183+hammad42662@users.noreply.github.com>
2024-09-16 02:10:10 +0100

    Update Milestones.jsx

--- src/app/components/tabs/Milestones.jsx
+++ src/app/components/tabs/Milestones.jsx
@@ -55,7 +55,7 @@ export default function Milestones() {
               <div className="absolute -left-[40.9px] top-5 bg-blue-600 rounded-full h-4 w-4"></div>
               <div className=" flex flex-col items-start">
                 <h3 className="text-xl font-bold mb-2">
-                  Frontend Developer Intern
+                  Front-end Developer Intern
                 </h3>
                 <p>Convent Generation IT</p>
                 <span className=" text-gray-400 text-sm font-light w-full text-left ">
@@ -66,7 +66,7 @@ export default function Milestones() {
             <li className="relative border-b border-dashed border-gray-400 py-4 last:border-none">
               <div className="absolute -left-[40.9px] top-5 bg-blue-600 rounded-full h-4 w-4"></div>
               <div className=" flex flex-col items-start">
-                <h3 className="text-xl font-bold mb-2">Junior Web Developer</h3>
+                <h3 className="text-xl font-bold mb-2">Junior Front-end Developer</h3>
                 <p>Convent Generation IT</p>
                 <span className=" text-gray-400 text-sm font-light w-full text-left ">
                   August,2019 -January 2020
