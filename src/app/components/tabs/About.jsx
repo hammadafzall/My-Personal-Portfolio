edb517c580e7f3d5e2fc6e74ec6fe1f26c6bb15c
Hammad Afzal <127996183+hammadafzall@users.noreply.github.com>
2025-07-03 18:48:26 +0100

    Update About.jsx

--- src/app/components/tabs/About.jsx
+++ src/app/components/tabs/About.jsx
@@ -7,17 +7,15 @@ export default function About() {
       </h3>
       <div className="mt-4">
         <p className="text-gray-500 text-lg leading-10 ">
-          As a Front-End Developer with
+          As a Full-Stack Developer with
           <span className=" ml-1 mr-1 text-2xl bg-gradient-to-r from-blue-500 to-blue-900 bg-clip-text text-transparent font-black">
-            3+
+            3 +
           </span>
-          years of experience, I specialize in crafting digital solutions that
-          drive results. My expertise lies in creating visually engaging,
-          user-centric websites, backed by a solid understanding of back-end
-          technologies. My experience at Convent Generation IT and my successful
-          freelance projects have honed my ability to translate client needs
-          into impactful online experiences. I&apos;m passionate about building
-          seamless, high-performance websites that deliver tangible value.
+          years of experience, I specialize in crafting digital solutions that drive results. 
+          My expertise lies in creating visually engaging, user-centric websites, 
+          backed by a solid understanding of back-end technologies. Through both freelance projects and professional roles, 
+          I’ve honed my ability to translate client needs into impactful online experiences. I’m passionate about building seamless, 
+          high-performance websites that deliver tangible value.
         </p>
       </div>
     </div>
